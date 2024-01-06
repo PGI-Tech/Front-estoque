5be0613cdc46591e3a9eb95d9b2e359c9a8f1244
igordrsilva <igdouglas.silva@gmail.com>
2024-01-06 19:49:24 -0300

    frontend updates

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Input from "../../components/Input";
-import Select from "../../components/Select";
 import Button from "../../components/Button";
 import * as C from "./styles";
 import { Link, useNavigate } from "react-router-dom";
