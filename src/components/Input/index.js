98b397be3f1107465f57f513af6f4c3a7d57ec76
igordrsilva <igdouglas.silva@gmail.com>
2024-01-11 20:33:10 -0300

    updates

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,15 +1,3 @@
-import React from "react";
-import * as C from "./styles";
-
-const Input = ({ type, placeholder, value, onChange }) => {
-  return (
-    <C.Input
-      value={value}
-      onChange={onChange}
-      type={type}
-      placeholder={placeholder}
-    />
-  );
-};
-
-export default Input;
+version https://git-lfs.github.com/spec/v1
+oid sha256:33be3fa819abcbd13319a7a51dec6a0bf25291b74439717f565ddbd5456479ae
+size 292
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,3 @@
-import React, { useState } from "react";
-import Sidebar from "../../components/Sidebar";
-
-const Home = () => {
-
-  return(
-    <div>
-      <div><Sidebar></Sidebar></div>
-      <div></div>
-    </div>
-  );
-};
-
-export default Home;
+version https://git-lfs.github.com/spec/v1
+oid sha256:83e0d8e8ba9e00c577b3feec921f392b774de662fd9265a37d8891c33ad2a430
+size 240
